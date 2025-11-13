e6577cb8fbf3d85c7768a36e7b0dfca2daf3467c
emuriu125-cmd <emuriu125@gmail.com>
2025-11-13 12:35:32 +0300

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -199,7 +199,6 @@ elif page == "🌱 Crop Water Guide":
     col3, col4 = st.columns(2)
     with col3:
         st.session_state["efficiency_percent_cw"] = st.number_input("Irrigation Efficiency (%)", value=st.session_state["efficiency_percent_cw"], min_value=1, max_value=100)
-        st.session_state["c_source_cap"] = st.number_input("Source Capacity (Liters/hour)", min_value=1.0, value=st.session_state["c_source_cap"])
     st.session_state["display_supply_results"] = True
 
     with st.expander("📱 Need Help Getting These Values?"):
